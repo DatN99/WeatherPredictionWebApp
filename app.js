77f6eed0031e50134b7c229efa2eb8b49be98e3a
dtnguyen01 <dtnguyen01@email.wm.edu>
2021-12-23 17:39:02 -0500

    added rps

--- app.js
+++ app.js
@@ -7,9 +7,7 @@ var port = process.env.PORT || 8000;
 app.use(express.static("public\\rock-paper-scissor"));
 
 app.get("/", function(req, res) {
-    res.send("Hello World");
-
-
+    res.send("Hello Worldddddd");
 });
 
 app.listen(port);
