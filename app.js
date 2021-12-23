e9360e9e784aae512a8956f639fddbf52cc2ab9a
dtnguyen01 <dtnguyen01@email.wm.edu>
2021-12-23 17:26:40 -0500

    added html

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 var express = require("express");
-
 var app = express();
-
 var port = process.env.PORT || 8000;
+
+
+app.use(express.static("public"))
 
 app.get("/", function(req, res) {
     res.send("Hello World");
+
+
 });
 
 app.listen(port);
+
