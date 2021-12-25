045579cf1c0f8f148ac9cd3e9fa0404c4413a7fd
dtnguyen01 <dtnguyen01@email.wm.edu>
2021-12-24 22:04:50 -0500

    working app that gets weather in fairfax every 1 hour

--- public/weather.js
+++ public/weather.js
@@ -1,8 +1,25 @@
+let temp_list = [];
 
 
-fetch('https://api.openweathermap.org/data/2.5/weather?zip=22031,us&appid=ed558cf47baba6f38e93e70b4e82d2e0')
-  .then(response => response.json())
-  .then(data => console.log(data))
+get_data = async () => {
+   const fetch_api = await fetch('https://api.openweathermap.org/data/2.5/weather?zip=22031,us&appid=ed558cf47baba6f38e93e70b4e82d2e0')
+       .then(response => response.json())
+       .then(data => temp_list.push(data.main.temp))
 
-.catch("Can't get weather info");
+   console.log(temp_list);
+   console.log("temp list changed")
+
+ }
+
+
+get_data();
+console.log("get_data called")
+
+setInterval(get_data, 3600000)
+console.log("setInterval called")
+
+
+
+
+
 
